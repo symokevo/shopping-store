dd904599a5478c72403e322fc692b08c512289d6
Simon Rabuogi <symokevo@gmail.com>
2024-06-05 02:12:14 +0300

    Design the footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from './layout/top-bar/top-bar.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TopBarComponent],
+  imports: [CommonModule, RouterOutlet, TopBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
